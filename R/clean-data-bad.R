c430ff712dca7d0ee0183dbe505a469a9927818c
Annika Gompers <annika.gompers@gmail.com>
2023-08-14 11:26:34 -0400

    ran bad data cleaning script

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,8 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+
+setwd("C:/Users/agomper/OneDrive - Emory University/Documents/Year 2/EPI 590R/epi590r-in-class/data/raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
